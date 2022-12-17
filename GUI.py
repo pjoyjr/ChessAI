a911c9cde24ee0e8ebbeadb41218380ae700808f
pjoyjr <37516097+pjoyjr@users.noreply.github.com>
2022-12-17 16:10:03 -0500

    prints board

--- GUI.py
+++ GUI.py
@@ -1,15 +1,61 @@
 import pygame as p
+from math import floor
 from settings import *
+from sys import stdout
 
 class GUI:
-    def __init__(self):
+    def __init__(self, __board):
         p.init()
+        self.screen = p.display.set_mode((GUI_WIDTH, GUI_HEIGHT))
+        self.clock = p.time.Clock()
+        stdout.flush()
         self.piece_images = {}
         self.loadImages()
-
+        self.board = __board
 
     def loadImages(self):
-        pieces = ['p', 'r', 'n', 'b', 'q', 'k', 'wp', 'wr', 'wn', 'wb', 'wq', 'wk']
+        imgPath = "png\\"
+        pieceMapping = {
+            "b": imgPath + "b.png",
+            "k": imgPath + "k.png",
+            "n": imgPath + "n.png",
+            "p": imgPath + "p.png",
+            "q": imgPath + "q.png",
+            "r": imgPath + "r.png",
+            "B": imgPath + "Bb.png",
+            "K": imgPath + "Bk.png",
+            "N": imgPath + "Bn.png",
+            "P": imgPath + "Bp.png",
+            "Q": imgPath + "Bq.png",
+            "R": imgPath + "Br.png",
+        }
+        pieces = ['p', 'r', 'n', 'b', 'q', 'k', 'P', 'R', 'N', 'B', 'Q', 'K']
         for piece in pieces:
-            self.piece_images[piece] = p.transform.scale(p.image.load("png/" + piece + ".png"), (GUI_SQ_SIZE, GUI_SQ_SIZE))
+            self.piece_images[piece] = p.transform.scale(p.image.load(pieceMapping[piece]), (GUI_SQ_SIZE, GUI_SQ_SIZE))
 
+    
+    def drawSquares(self):
+        colors = [p.Color(BOARD_COLOR_1), p.Color(BOARD_COLOR_2)]
+        for row in range(BOARD_DIM):
+            for col in range(BOARD_DIM):
+                color = colors[((col+row)%2)]
+                p.draw.rect(self.screen, color, p.Rect(col*GUI_SQ_SIZE, row*GUI_SQ_SIZE, GUI_SQ_SIZE, GUI_SQ_SIZE))
+	
+    def drawPieces(self):
+        for i in range(TOTAL_SQUARES):
+            rank = floor(i / 8)
+            file = i % 8
+            piece = self.board.piece_at(i)
+            if piece != None:
+                self.screen.blit(self.piece_images[str(piece)], p.Rect(file*GUI_SQ_SIZE, rank*GUI_SQ_SIZE, GUI_SQ_SIZE, GUI_SQ_SIZE))     
+
+    def showBoard(self):
+        running = True
+        while running:
+            for e in p.event.get():
+                if e.type == p.QUIT:
+                    running = False
+            self.drawSquares()
+            self.drawPieces()
+            self.clock.tick(FPS)
+            p.display.flip()
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import chess
+from GUI import GUI
 #https://pypi.org/project/chess/
 
 
 def main():
     board = chess.Board()
+    gui = GUI(board)
+    gui.showBoard()
 
-    board.legal_moves
+    # board.legal_moves
 
-    chess.Move.from_uci("a8a1") in board.legal_moves
+    # chess.Move.from_uci("a8a1") in board.legal_moves
 
-    board.push_san("e4")
-    board.push_san("e5")
-    board.push_san("Qh5")
-    board.push_san("Nc6")
-    board.push_san("Bc4")
-    board.push_san("Nf6")
-    board.push_san("Qxf7")
+    # board.push_san("e4")
+    # board.push_san("e5")
+    # board.push_san("Qh5")
+    # board.push_san("Nc6")
+    # board.push_san("Bc4")
+    # board.push_san("Nf6")
+    # board.push_san("Qxf7")
 
-    board.is_checkmate()
+    # board.is_checkmate()
 
     print(board)
 
