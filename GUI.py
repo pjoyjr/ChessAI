967c93a1a13f9cc36c99e0411268c4399e5f5907
pjoyjr <37516097+pjoyjr@users.noreply.github.com>
2022-12-19 21:50:52 -0500

    added gui space for graveyard and sidebar

--- GUI.py
+++ GUI.py
@@ -6,7 +6,9 @@ from sys import stdout
 class GUI:
     def __init__(self, __board):
         p.init()
-        self.screen = p.display.set_mode((GUI_BOARD_WIDTH, GUI_BOARD_HEIGHT+2*GUI_GRAVEYARD_HEIGHT))
+        width = GUI_BOARD_WIDTH+GUI_SPACING_COLUMN_WIDTH*2+GUI_SIDEBAR_WIDTH
+        height = GUI_BOARD_HEIGHT+2*GUI_GRAVEYARD_HEIGHT
+        self.screen = p.display.set_mode((width, height))
         self.clock = p.time.Clock()
         stdout.flush()
         self.piece_images = {}
@@ -28,25 +30,40 @@ class GUI:
         color = p.Color("black")
         for row in range(1,BOARD_DIM):
             for col in range(1,BOARD_DIM):
-                p.draw.line(self.screen, color, (col*GUI_SQ_SIZE, GUI_GRAVEYARD_HEIGHT), (col*GUI_SQ_SIZE, GUI_BOARD_WIDTH+GUI_GRAVEYARD_HEIGHT), 2)
+                #separate cols
+                x_start = GUI_SPACING_COLUMN_WIDTH+col*GUI_SQ_SIZE
+                x_end = GUI_SPACING_COLUMN_WIDTH+col*GUI_SQ_SIZE
+                y_start = GUI_GRAVEYARD_HEIGHT
+                y_end = GUI_BOARD_WIDTH+GUI_GRAVEYARD_HEIGHT
+                p.draw.line(self.screen, color, (x_start, y_start), (x_end, y_end), 2)
                 #separate rows
-                p.draw.line(self.screen, color, (0, GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE), (GUI_BOARD_HEIGHT, GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE), 2)
+                x_start = GUI_SPACING_COLUMN_WIDTH
+                x_end = GUI_SPACING_COLUMN_WIDTH+GUI_BOARD_HEIGHT
+                y_start = GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE
+                y_end = GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE
+                p.draw.line(self.screen, color, (x_start, y_start), (x_end, y_end), 2)
 
     def drawLettersNumbers(self):
         for row in range(0,BOARD_DIM):
             font = p.font.SysFont(None, 20)
             text = font.render(str(abs(row-8)), True, p.Color("black"))
-            self.screen.blit(text, (0, GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE+5))
+            x_pos = GUI_SPACING_COLUMN_WIDTH
+            y_pos = GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE+5
+            self.screen.blit(text, (x_pos, y_pos))
         
         for col in range(0,BOARD_DIM):
             font = p.font.SysFont(None, 20)
             text = font.render(FILE_INT_TO_STR_MAPPING[col], True, p.Color("black"))
-            self.screen.blit(text, (col*GUI_SQ_SIZE+2, GUI_GRAVEYARD_HEIGHT+BOARD_DIM*GUI_SQ_SIZE-15))
+            x_pos = GUI_SPACING_COLUMN_WIDTH+col*GUI_SQ_SIZE+2
+            y_pos = GUI_GRAVEYARD_HEIGHT+BOARD_DIM*GUI_SQ_SIZE-15
+            self.screen.blit(text, (x_pos, y_pos))
     
     def drawHighlightMoves(self):
         color = p.Color(BOARD_COLOR_HIGHLIGHTED_TILES)
         for move in self.highlighted_tiles:
-            p.draw.rect(self.screen, color, p.Rect(FILE_STR_TO_INT_MAPPING[move[0]]*GUI_SQ_SIZE, GUI_GRAVEYARD_HEIGHT+(8-int(move[1]))*GUI_SQ_SIZE, GUI_SQ_SIZE, GUI_SQ_SIZE))
+            left_pos = GUI_SPACING_COLUMN_WIDTH+FILE_STR_TO_INT_MAPPING[move[0]]*GUI_SQ_SIZE
+            top_pos = GUI_GRAVEYARD_HEIGHT+(8-int(move[1]))*GUI_SQ_SIZE
+            p.draw.rect(self.screen, color, p.Rect(left_pos, top_pos, GUI_SQ_SIZE, GUI_SQ_SIZE))
 				
 
     def drawSquares(self):
@@ -54,7 +71,9 @@ class GUI:
         for row in range(BOARD_DIM):
             for col in range(BOARD_DIM):
                 color = colors[((col+row)%2)]
-                p.draw.rect(self.screen, color, p.Rect(col*GUI_SQ_SIZE, GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE, GUI_SQ_SIZE, GUI_SQ_SIZE))
+                left_pos = GUI_SPACING_COLUMN_WIDTH+col*GUI_SQ_SIZE
+                top_pos = GUI_GRAVEYARD_HEIGHT+row*GUI_SQ_SIZE
+                p.draw.rect(self.screen, color, p.Rect(left_pos, top_pos, GUI_SQ_SIZE, GUI_SQ_SIZE))
 	
     def drawPieces(self):
         for i in range(TOTAL_SQUARES):
@@ -62,7 +81,9 @@ class GUI:
             file = i % 8
             piece = self.board.piece_at(i)
             if piece != None:
-                self.screen.blit(self.piece_images[str(piece)], p.Rect(file*GUI_SQ_SIZE, GUI_GRAVEYARD_HEIGHT+rank*GUI_SQ_SIZE, GUI_SQ_SIZE, GUI_SQ_SIZE))     
+                left_pos = GUI_SPACING_COLUMN_WIDTH+file*GUI_SQ_SIZE
+                top_pos = GUI_GRAVEYARD_HEIGHT+rank*GUI_SQ_SIZE
+                self.screen.blit(self.piece_images[str(piece)], p.Rect(left_pos, top_pos, GUI_SQ_SIZE, GUI_SQ_SIZE))     
 
     def showBoard(self):
         for e in p.event.get():
@@ -70,28 +91,31 @@ class GUI:
                 return False
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos() # (x, y) location of mouse
-                file_int = location[0]//GUI_SQ_SIZE
-                file_str = FILE_INT_TO_STR_MAPPING[file_int]
-                rank_int = (location[1]-GUI_GRAVEYARD_HEIGHT)//GUI_SQ_SIZE
-                rank_str = str(8-rank_int)
-                tile_name = file_str + rank_str
-                if self.sqSelected == tile_name: #click same square twice
-                    self.sqSelected = None
-                    self.playerClicks = []
-                    self.highlighted_tiles = []
-                else: #clicked a new square
-                    self.highlighted_tiles = []
-                    self.sqSelected = tile_name
-                    self.playerClicks.append(self.sqSelected)
-                    print(self.playerClicks)
-                    for move in self.board.legal_moves:
-                        from_tile = str(move)[0] + str(move)[1]
-                        if (self.sqSelected == from_tile):
-                            self.highlighted_tiles.append(str(move)[2] + str(move)[3])
+                x_mouse = location[0]
+                y_mouse = location[1]
+                if(x_mouse > GUI_BOARD_WIDTH_OFFSET and x_mouse < GUI_BOARD_WIDTH + GUI_BOARD_WIDTH_OFFSET) and (y_mouse > GUI_BOARD_HEIGHT_OFFSET and y_mouse < GUI_BOARD_HEIGHT + GUI_BOARD_HEIGHT_OFFSET):
+                    file_int = (location[0]-GUI_BOARD_WIDTH_OFFSET)//GUI_SQ_SIZE
+                    file_str = FILE_INT_TO_STR_MAPPING[file_int]
+                    rank_int = (location[1]-GUI_BOARD_HEIGHT_OFFSET)//GUI_SQ_SIZE
+                    rank_str = str(8-rank_int)
+                    tile_name = file_str + rank_str
+                    if self.sqSelected == tile_name: #click same square twice
+                        self.sqSelected = None
+                        self.playerClicks = []
+                        self.highlighted_tiles = []
+                    else: #clicked a new square
+                        self.highlighted_tiles = []
+                        self.sqSelected = tile_name
+                        self.playerClicks.append(self.sqSelected)
+                        print(self.playerClicks)
+                        for move in self.board.legal_moves:
+                            from_tile = str(move)[0] + str(move)[1]
+                            if (self.sqSelected == from_tile):
+                                self.highlighted_tiles.append(str(move)[2] + str(move)[3])
 
-                # if len(self.playerClicks) == 2:
-                #     print("Make Move")
-                #     self.playerClicks = []
+                    # if len(self.playerClicks) == 2:
+                    #     print("Make Move")
+                    #     self.playerClicks = []
 					
         self.drawSquares()
         self.drawHighlightMoves()
