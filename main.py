37f76efe83c4a1f98fd347c997f6d56bdb296aab
pjoyjr <37516097+pjoyjr@users.noreply.github.com>
2022-12-23 18:39:38 -0500

    random move

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import chess
 from GUI import GUI
+import random
+from settings import WHITEAI, BLACKAI
 #https://pypi.org/project/chess/
 
+
+def isAI(color: str):
+    if (color == "White" and WHITEAI) or (color == "Black" and BLACKAI):
+        return True
+    return False
 
 def whichColor(boardTurn: bool) -> str:
     if boardTurn:
         return "White"
-    return "black"
-
+    return "Black"
 
 def main():
     board = chess.Board()
     gui = GUI(board)
     while gui.running and board.legal_moves:
-        # print(whosTurn(board.turn))
-        move = gui.showBoard()
+        move = None
+        if isAI(whichColor(board.turn)):
+            move = random.choice(list(board.legal_moves))
+        else:
+            move = gui.showBoard()
         if move != None:
             board.push(move)
     print(board.outcome)
